a6b4d226c78f47bc39623afbabbb62a04eb85720
Danil Deineka <tripledpadik@gmail.com>
2023-03-25 00:55:21 +0200

    make markup and conected it with backend

--- src/index.js
+++ src/index.js
@@ -1,44 +1,81 @@
-import debounce from 'lodash.debounce';
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 3000;
+const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const info = document.querySelector('.country-info');
 
-input.addEventListener('input', debounce((evt) => {
-    fetchCountries(evt)  
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
-}, DEBOUNCE_DELAY));
+input.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
 
-function fetchCountries(event) {
+function inputHandler(event) {
   const countryName = event.target.value.trim();
 
+  if (!countryName) {
+    cleanMarkup(list);
+    cleanMarkup(info);
+    return;
+  }
 
-
-  return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
-  .then(
-    response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-
-        return response.json()
-    }
-  )
+  fetchCountries(countryName)
+    .then(data => chooseTypeOfMarkup(data))
+    .catch(error => onError(error));
 }
 
+function cleanMarkup() {
+  list.innerHTML = '';
+  info.innerHTML = '';
+}
 
-// name.official - повна назва країни
-// capital - столиця
-// population - населення
-// flags.svg - посилання на зображення прапора
-// languages - масив мов
+function onError(error) {
+  cleanMarkup();
+  Notiflix.Notify.failure('Qui timide rogat docet negare');
+}
 
-function getDataValue(info){
-    return info.map(({name, capital, population, flags, languages}) => {
-        console.log(flags.svg);
-    });
+function chooseTypeOfMarkup(data) {
+  cleanMarkup();
+  if (data.length >= 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
+  }
+
+  if (data.length === 1) {
+    info.innerHTML = createCountryInfo(data);
+  } else {
+    list.innerHTML = createCountryList(data);
+  }
+}
+
+function createCountryList(data) {
+  return data
+    .map(
+      ({ name, capital, population, flags, languages }) =>
+        `<li style="display: flex; gap: 15px; align-items: center;">
+            <img src="${flags.svg}" height="32px" width="50px">
+            <p style="font-size: 20px; ">${name.official}</p>
+        </li>`
+    )
+    .join('');
+}
+
+function createCountryInfo(data) {
+  return data
+    .map(({ name, capital, population, flags, languages }) => {
+      const lang = Object.values(languages);
+
+      return `
+        <li style="display: flex; gap: 15px; align-items: center;">
+            <img src="${flags.svg}" height="32px" style="display: flex;">
+            <h1 style="margin: 0">${name.official}</h1>
+        </li>
+        <p><b>Capital:</b> ${capital}</p>
+        <p><b>Population:</b> ${population}</p>
+        <p><b>Languages:</b> ${lang}</p>`;
+    })
+    .join('');
 }
