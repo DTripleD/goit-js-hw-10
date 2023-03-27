b1f8f6078b3f07f80782fd719efbfed59cbee22d
Danil Deineka <tripledpadik@gmail.com>
2023-03-27 13:50:58 +0300

    Bug fix index.js

--- src/index.js
+++ src/index.js
@@ -1,89 +1,6 @@
 import './css/styles.css';
-// import debounce from 'lodash.debounce';
-// import { fetchCountries } from './js/fetchCountries';
-// import Notiflix from 'notiflix';
-
-// const DEBOUNCE_DELAY = 300;
-
-// const input = document.querySelector('#search-box');
-// const list = document.querySelector('.country-list');
-// const info = document.querySelector('.country-info');
-
-// input.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
-
-// function inputHandler(event) {
-//   const countryName = event.target.value.trim();
-
-//   if (!countryName) {
-//     cleanMarkup(list);
-//     cleanMarkup(info);
-//     return;
-//   }
-
-//   fetchCountries(countryName)
-//     .then(data => chooseTypeOfMarkup(data))
-//     .catch(error => onError(error));
-// }
-
-// function cleanMarkup() {
-//   list.innerHTML = '';
-//   info.innerHTML = '';
-// }
-
-// function onError(error) {
-//   cleanMarkup();
-//   Notiflix.Notify.failure('Qui timide rogat docet negare');
-// }
-
-// function chooseTypeOfMarkup(data) {
-//   cleanMarkup();
-//   if (data.length >= 10) {
-//     Notiflix.Notify.info(
-//       'Too many matches found. Please enter a more specific name.'
-//     );
-//     return;
-//   }
-
-//   if (data.length === 1) {
-//     info.innerHTML = createCountryInfo(data);
-//   } else {
-//     list.innerHTML = createCountryList(data);
-//   }
-// }
-
-// function createCountryList(data) {
-//   return data
-//     .map(
-//       ({ name, capital, population, flags, languages }) =>
-//         `<li style="display: flex; gap: 15px; align-items: center;">
-//             <img src="${flags.svg}" height="32px" width="50px">
-//             <p style="font-size: 20px; ">${name.official}</p>
-//         </li>`
-//     )
-//     .join('');
-// }
-
-// function createCountryInfo(data) {
-//   return data
-//     .map(({ name, capital, population, flags, languages }) => {
-//       const lang = Object.values(languages);
-
-//       return `
-//         <li style="display: flex; gap: 15px; align-items: center;">
-//             <img src="${flags.svg}" height="32px" style="display: flex;">
-//             <h1 style="margin: 0">${name.official}</h1>
-//         </li>
-//         <p><b>Capital:</b> ${capital}</p>
-//         <p><b>Population:</b> ${population}</p>
-//         <p><b>Languages:</b> ${lang}</p>`;
-//     })
-//     .join('');
-// }
-
-
-
-import { fetchCountries } from './js/fetchCountries.js';
 import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
@@ -98,8 +15,7 @@ function makeMarkup(event) {
   const countryName = event.target.value.trim();
 
   if (!countryName) {
-    listCountry.innerHTML = '';
-    infoCountry.innerHTML = '';
+    return cleanMarkup();
   }
 
   fetchCountries(countryName)
@@ -124,27 +40,30 @@ function chooseMarkup(data) {
     Notiflix.Notify.warning(
       'Too many matches found. Please enter a more specific name.'
     );
-  } else if (data.length >= 2) {
-    listCountry.insertAdjacentHTML('beforeend', list(data));
-  } else if (data.length === 1) {
-    infoCountry.insertAdjacentHTML('beforeend', info(data));
+    return;
+  }
+
+  if (data.length >= 2) {
+    listCountry.innerHTML = createCountryList(data);
+  } else {
+    infoCountry.innerHTML = createCountryInfo(data);
   }
 }
 
-function list(name) {
-  return name
+function createCountryList(data) {
+  return data
     .map(
       ({ name, flags }) =>
-        `<li style='display: flex; align-items: center; gap: 20px'>
-      <img src='${flags.svg}' alt='${flags.alt}' style='width: 50px'>
-      <p>${name.official}</p>
-    </li>`
+        `<li style="display: flex; gap: 15px; align-items: center;">
+          <img src='${flags.svg}' alt='${flags.alt}' height="32px" width="50px">
+          <p style="font-size: 20px;">${name.official}</p>
+        </li>`
     )
     .join('');
 }
 
-function info(name) {
-  return name
+function createCountryInfo(data) {
+  return data
     .map(
       ({ name, capital, population, flags, languages }) =>
         `<li style='display: flex; align-items: center; gap: 30px'>
@@ -153,7 +72,7 @@ function info(name) {
     </li>
     <p><b>Capital:</b> ${capital}</p>
     <p><b>Population:</b> ${population}</p>
-    <p><b>Languages:</b> ${Object.values(languages).join('')}</p>
+    <p><b>Languages:</b> ${Object.values(languages)}</p>
     `
     )
     .join('');
